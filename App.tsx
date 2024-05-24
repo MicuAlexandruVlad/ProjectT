ebb43016dfeb719643a06ebebf6f5b999f9d8fed
Alexandru-Vlad Micu <vlad15235@gmail.com>
2024-05-24 15:19:50 +0300

    App color theme syncs with the user's device

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Appearance, Platform, StyleSheet, Text, UIManager, View } from 'react-native'
+import store from './src/redux/store'
+import { useEffect } from 'react'
+import { setTheme } from './src/redux/slices/uiController'
 
 export default function App() {
-  return (
-    <View style={ styles.container }>
-      
-    </View>
-  );
+	const dispatch = store.dispatch
+
+	useEffect(() => {
+		dispatch(setTheme(Appearance.getColorScheme() === 'dark'))
+
+		if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+			UIManager.setLayoutAnimationEnabledExperimental(true)
+		}
+	}, [])
+	
+	return (
+		<View style={ styles.container }>
+		
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})

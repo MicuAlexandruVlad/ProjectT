c8a6669ec5b95c932ff3119c0753db469d5a58da
Alexandru-Vlad Micu <vlad15235@gmail.com>
2024-05-28 14:39:52 +0300

    feat: Added basic MainScreen component

---
+++ src/ui/screens/MainScreen/MainScreen.tsx
@@ -0,0 +1,31 @@
+import { NavigationProp } from "@react-navigation/native"
+import React from "react"
+import RootStackParamList from "../../../navigation/RootStackParamList"
+import { View, Text, StyleSheet } from "react-native"
+import Theme from "../../../data/models/Theme"
+import ThemeUtils from "../../../utils/ThemeUtils"
+import { useSelector } from "react-redux"
+
+type Props = {
+    navigation: NavigationProp<RootStackParamList>
+}
+
+const MainScreen: React.FC<Props> = ({ navigation }): React.JSX.Element => {
+    const theme = ThemeUtils.getTheme(useSelector)
+
+    const styles = styleSheet(theme)
+
+    return (
+        <View>
+            <Text>Main Screen</Text>
+        </View>
+    )
+}
+
+const styleSheet = (theme: Theme) => StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
+
+export default MainScreen

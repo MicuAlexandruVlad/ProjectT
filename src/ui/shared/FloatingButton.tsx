1d78822e656078b039a3448c72b9ef68b06e1b33
Alexandru-Vlad Micu <vlad15235@gmail.com>
2024-06-01 20:14:03 +0300

    feat: Add FloatingButton component

---
+++ src/ui/shared/FloatingButton.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import Theme from "../../data/models/Theme"
+import { MaterialIcons } from "@expo/vector-icons"
+import ThemeUtils from "../../utils/ThemeUtils"
+import { useSelector } from "react-redux"
+
+type Props = {
+    onPress: () => void
+}
+
+const FloatingButton: React.FC<Props> = ({ onPress }): React.JSX.Element => {
+    const theme = ThemeUtils.getTheme(useSelector)
+
+    const styles = styleSheet(theme)
+
+    const handlePress = React.useCallback(() => {
+        onPress()
+    }, [])
+    
+    return (
+        <TouchableOpacity onPress={ handlePress } style={ styles.container }>
+            <MaterialIcons name="post-add" size={ 20 } color={ theme.colors.onTertiary } />
+        </TouchableOpacity>
+    )
+}
+
+const styleSheet = (theme: Theme) => StyleSheet.create({
+    container: {
+        position: "absolute",
+        bottom: 24,
+        right: 24,
+        backgroundColor: theme.colors.tertiary,
+        padding: 16,
+        borderRadius: 100
+    }
+})
+
+export default memo(FloatingButton)

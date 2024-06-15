ba7faf596d724ec3f3d7226b4f85ab0395588aec
Alexandru-Vlad Micu <vlad15235@gmail.com>
2024-06-15 14:09:58 +0300

    Changed Post object structure

--- src/data/models/Post.ts
+++ src/data/models/Post.ts
@@ -18,42 +18,15 @@ export interface Engagement {
 }
 
 export default interface Post {
+    id: number
     user: PostUser
     content: string
     media?: Media
     metadata: {
-        postId: string
         timestamp: Date
         location?: string
     }
     engagement: Engagement
     hashtags?: string[]
     mentions?: string[]
-}
-
-const examplePost: Post = {
-    user: {
-        username: 'user123',
-        displayName: 'User OneTwoThree',
-        userId: '00123',
-        profilePictureUrl: 'http://example.com/profile.jpg',
-    },
-    content: 'This is an example tweet!',
-    media: {
-        imageUrl: 'http://example.com/image.jpg',
-        videoUrl: 'http://example.com/video.mp4',
-        // linkUrl can be included if there's a link in the content
-    },
-    metadata: {
-        postId: 'post00123',
-        timestamp: new Date(),
-        location: 'Constanța, Romania', // Optional
-    },
-    engagement: {
-        reposts: 10,
-        likes: 100,
-        comments: 5,
-    },
-    hashtags: ['#example', '#typescript'],
-    mentions: ['@anotheruser'],
 }

1184cce89e7ce85b21d6ef687285c9365a7ddd4d
Alexandru-Vlad Micu <vlad15235@gmail.com>
2024-07-05 15:12:44 +0300

    Removed BlurView from Loader

--- src/ui/shared/Loader.tsx
+++ src/ui/shared/Loader.tsx
@@ -1,4 +1,3 @@
-import { BlurView } from "@react-native-community/blur"
 import AnimatedLottieView from "lottie-react-native"
 import React from "react"
 import { Modal, StyleSheet, View } from "react-native"
@@ -32,18 +31,6 @@ const Loader: React.FC<Props> = ({ loading }) => {
                     justifyContent: 'center'
                 }}
             >
-                <BlurView
-                    blurAmount={ 1 }
-                    reducedTransparencyFallbackColor={ theme.colors.background }
-                    blurType={ uiController.isDarkTheme ? 'dark' : 'light' }
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0
-                    }}
-                />
                 <AnimatedLottieView
                     source={ loadingAnim }
                     style={ styles.animation }
